6fcc04e06a73544802b8221d0033cbb2381de730
Ilya Hrankouski <ilya.hrankouski@gmail.com>
2013-03-28 00:59:34 +0300

    Passed tests

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,21 @@
 (ns clojure-course-task02.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.core.reducers :as r]))
 
+(defn make-filesearch [suitable-file?]
+  (fn search [f]
+    (let [N 10
+          cf (r/monoid into vector)
+          rf #(r/reduce conj %1 (search %2))]
+      (concat (r/fold N cf rf (vec (.listFiles f)))
+              (when (suitable-file? f) [(.getName f)])))))  
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (let [ptrn (re-pattern file-name)
+        pred #(->> % .getName (re-find ptrn) nil? not)
+        search (make-filesearch pred)]
+    (search (clojure.java.io/file path))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
