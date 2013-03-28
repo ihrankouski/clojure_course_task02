c5d1cf3952572b5653e03ecde30de8940467d2ad
Ilya Hrankouski <ilya.hrankouski@gmail.com>
2013-03-28 12:05:07 +0300

    Reusability improvements

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  ; :aot :all
+  :aot :all
   :main clojure-course-task02.core)
--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -2,19 +2,21 @@
   (:gen-class)
   (:require [clojure.core.reducers :as r]))
 
-(defn make-filesearch [suitable-file?]
+(defn make-search [predicate? producer visitor]
   (fn search [f]
     (let [N 10
           cf (r/monoid into vector)
           rf #(r/reduce conj %1 (search %2))]
-      (concat (r/fold N cf rf (vec (.listFiles f)))
-              (when (suitable-file? f) [(.getName f)])))))  
+      (concat (r/fold N cf rf (producer f))
+              (when (predicate? f) [(visitor f)])))))
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  (let [ptrn (re-pattern file-name)
-        pred #(->> % .getName (re-find ptrn) nil? not)
-        search (make-filesearch pred)]
+  (let [p (re-pattern file-name)
+        pred #(->> % .getName (re-find p) nil? not)
+        prdc #(vec (.listFiles %))
+        vstr #(.getName %)
+        search (make-search pred prdc vstr)]
     (search (clojure.java.io/file path))))
 
 (defn usage []
